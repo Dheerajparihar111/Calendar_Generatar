cfced2361ec97eb7926c92cbaa85aa84a5f197d1
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-25 22:44:40 +0530

    added Days Of Week To  Table

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.Month;
 import java .util.*;
 public class Main {
     static void main(String[] args) {
@@ -8,24 +9,32 @@ public class Main {
         System.out.println("Enter The Month");
         String Month = input.nextLine();
         System.out.println(Month+" "+Year);
+        String []week ={"Mon","Tue","Wed","Thr","Fri","Sat","Sun"};
+        for (String a: week){
+            System.out.print(a+" ");
+        }
+        System.out.println();
         for (int i =1;i<=7;i++){
-            System.out.print("0"+i+" ");
+            System.out.print(" 0"+i+" ");
         }
         System.out.println();
         for (int j = 8;j<=14;j++){
             if (j < 10){
-                System.out.print("0"+j+" ");
+                System.out.print(" 0"+j+" ");
             }else {
-                System.out.print(j + " ");
+                System.out.print(" "+j + " ");
             }
         }
         System.out.println();
         for (int j = 15;j<=21;j++){
-            System.out.print(j+" ");
+            System.out.print(" "+j+" ");
         }
         System.out.println();
-        for (int j = 22;j<=30;j++){
-            System.out.print(j+" ");
+        for (int j = 22;j<=28;j++){
+            System.out.print(" "+j+" ");
         }
+        System.out.println();
+        System.out.print(" 29"+" "+" 30");
+
     }
 }
